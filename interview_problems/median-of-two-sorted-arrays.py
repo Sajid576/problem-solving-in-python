37df3c597a31fc8c058b5659e421f84715304ff0
Sajid576 <sajidahmed696@gmail.com>
2021-10-27 17:15:04 +0600

    median-of-sorted-arrays added

---
+++ interview_problems/median-of-two-sorted-arrays.py
@@ -0,0 +1,20 @@
+import math
+from typing import List
+
+
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    nums = [*nums1, *nums2]
+    nums.sort()
+    # print(nums)
+    if(len(nums) % 2 == 0):
+        indx1 = len(nums)//2 - 1
+        indx2 = len(nums)//2
+        val = (nums[indx1]+nums[indx2])/2
+        return val
+
+    else:
+        indx = math.floor(len(nums)/2)
+        return nums[indx]
+
+
+print(findMedianSortedArrays([2, 3], [1]))
