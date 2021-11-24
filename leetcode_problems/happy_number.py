83f4c9171ae7efc31620cdca1b795dfde80d3c92
Sajid576 <sajidahmed696@gmail.com>
2021-11-24 11:31:35 +0600

    Modify happy number

--- leetcode_problems/happy_number.py
+++ leetcode_problems/happy_number.py
@@ -13,11 +13,15 @@ def sumi(n):
 
 def isHappy(n: int) -> bool:
 	# Time and Space: O(logn), O(logn)
-	res = set()
-	while n != 1 and n not in res:
-		res.add(n)
-		n = sumi(n)
-	return n == 1
+    res = set()
+    while n != 1 and n not in res:
+        res.add(n)
+        n = sumi(n)
+
+    if(n == 1):
+        return True
+    else:
+        return False
 
 
 myTest.test(isHappy(19), True)
