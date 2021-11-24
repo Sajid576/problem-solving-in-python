97515b087a5d9a8ddeeddd0d088827df088957df
Sajid576 <sajidahmed696@gmail.com>
2021-11-24 11:22:37 +0600

    Add happy number

---
+++ leetcode_problems/happy_number.py
@@ -0,0 +1,25 @@
+'''https://leetcode.com/problems/happy-number/'''
+from typing import List
+import myTest
+
+
+def sumi(n):
+	res = 0
+	while n:
+		res += (n % 10)**2
+		n //= 10
+	return res
+
+
+def isHappy(n: int) -> bool:
+	# Time and Space: O(logn), O(logn)
+	res = set()
+	while n != 1 and n not in res:
+		res.add(n)
+		n = sumi(n)
+	return n == 1
+
+
+myTest.test(isHappy(19), True)
+
+myTest.test(isHappy(2), False)
